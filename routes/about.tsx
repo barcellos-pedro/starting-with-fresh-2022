4cbe645856036a990a74ecdcaef7e629184b04f4
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-11-10 12:55:51 -0300

    Add handler

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,3 +1,19 @@
+import { HandlerContext, Handlers } from '$fresh/server.ts';
+
+/**
+ * Example of a custom handler.
+ *
+ * Not required for the component route.
+ * Renders the component and set a header before returning the response.
+ */
+export const handler: Handlers = {
+  async GET(req: Request, ctx: HandlerContext) {
+    const response = await ctx.render();
+    response.headers.set('X-Custom-Header', 'About Page');
+    return response;
+  },
+};
+
 export default function AboutPage() {
   return (
     <main>
